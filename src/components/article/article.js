2dd2b7ef47d14417bc68846f1fc9e1fff28f8eb6
wormyn <87888767+wormyn@users.noreply.github.com>
2022-02-09 15:31:53 -0800

    added articles

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -87,6 +87,34 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'New Article',
+    date: 'Febuary 9th, 2022',
+    firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Squirtle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wartortle Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Blastoise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Caterpie Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Metapod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Butterfree
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Weedle Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Kakuna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beedrill Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit.`,
+
+    secondParagraph: `Pidgey Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pidgeotto Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Pidgeot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rattata Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Raticate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Spearow Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Fearow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ekans Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Arbok Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pikachu Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Raichu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sandshrew Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sandslash Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Nidorina Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoqueen Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`,
+
+    thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel
+        Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James
+        Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   }
 ];
 
@@ -116,27 +144,40 @@ const data = [
   Refresh the page to see the new article.
 */
 
-// function articleMaker(articleObj){
-//   const articleWrap = document.createElement('div')
-//   const articleTitle = document.createElement('h2')
-//   const articleDate = document.createElement('p')
-//   const articleP1 = document.createElement('p')
-//   const articleP2 = document.createElement('p')
-//   const articleP3 = document.createElement('p')
-//   const buttonExpand = document.createElement('span')
+function articleMaker(articleObj){
+  const articleWrap = document.createElement('div')
+  const articleTitle = document.createElement('h2')
+  const articleDate = document.createElement('p')
+  const articleP1 = document.createElement('p')
+  const articleP2 = document.createElement('p')
+  const articleP3 = document.createElement('p')
+  const buttonExpand = document.createElement('span')
 
-//   articleWrap.appendChild(articleTitle)
-//   articleWrap.appendChild(articleDate)
-//   articleWrap.appendChild(articleP1)
-//   articleWrap.appendChild(articleP2)
-//   articleWrap.appendChild(articleP3)
-//   articleWrap.appendChild(buttonExpand)
-
-//   articleWrap.classList.add('article');
-//   articleDate.classList.add('date')
-//   buttonExpand.classList.add('expandButton')
-
-//   articleObj.forEach()
+  articleWrap.classList.add('article');
+  articleDate.classList.add('date')
+  buttonExpand.classList.add('expandButton')
   
-// }
+  articleWrap.appendChild(articleTitle)
+  articleWrap.appendChild(articleDate)
+  articleWrap.appendChild(articleP1)
+  articleWrap.appendChild(articleP2)
+  articleWrap.appendChild(articleP3)
+  articleWrap.appendChild(buttonExpand)
 
+  articleTitle.textContent = articleObj.title
+  articleDate.textContent = articleObj.date
+  articleP1.textContent = articleObj.firstParagraph
+  articleP2.textContent = articleObj.secondParagraph
+  articleP3.textContent = articleObj.thirdParagraph
+  buttonExpand.textContent = '+'
+
+  buttonExpand.addEventListener('click', () => {
+    articleWrap.classList.toggle('article-open')
+  })
+
+  return articleWrap;
+}
+
+data.forEach(article => {
+  document.querySelector('div.articles').appendChild(articleMaker(article))
+})
